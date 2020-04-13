315cf618c5f7cc7d5e060f3a2ac9c20bc74e9f42
Arik Stewart <ams382@case.edu>
2020-04-13 00:22:36 -0400

    Updates

---
+++ precache-manifest.9073327ec8f2ccca87fa3e43008e36c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffcd4a46ddd91da23b9670c00beb7bc",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "3a4657b6d89d269919ed",
+    "url": "/React-Todo-App/static/css/main.250f35bf.chunk.css"
+  },
+  {
+    "revision": "9eaa6bfdc2d13561fb78",
+    "url": "/React-Todo-App/static/js/2.4bfa3af2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Todo-App/static/js/2.4bfa3af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4657b6d89d269919ed",
+    "url": "/React-Todo-App/static/js/main.d9dc9cd9.chunk.js"
+  },
+  {
+    "revision": "ee5b5327da8ab707a812",
+    "url": "/React-Todo-App/static/js/runtime-main.0893981d.js"
+  }
+]);
